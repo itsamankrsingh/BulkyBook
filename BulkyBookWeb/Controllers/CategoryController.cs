a876f4dc1dfb73faac430c991eb8f9f6aa0387f0
itsamankrsingh <itsamankrsingh@gmail.com>
2022-01-31 23:32:10 +0530

    initial commit

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET ACTION METHOD
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST ACTION METHOD
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
