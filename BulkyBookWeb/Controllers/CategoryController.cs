ae82cd34dd0337bdc7edd357e7c132a7db37a02d
itsamankrsingh <itsamankrsingh@gmail.com>
2022-02-01 12:29:06 +0530

    delete data from database

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -19,6 +19,10 @@ namespace BulkyBookWeb.Controllers
             return View(objCategoryList);
         }
 
+        /*
+         * CREATE
+         */
+
         //GET ACTION METHOD
         public IActionResult Create()
         {
@@ -63,6 +67,12 @@ namespace BulkyBookWeb.Controllers
             return View(categoryFromDb);
         }
 
+
+        /*
+         * UPDATE
+         */
+
+
         //POST ACTION METHOD
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -82,5 +92,44 @@ namespace BulkyBookWeb.Controllers
         }
 
 
+        /*
+         * DELETE
+         */
+
+
+        //GET ACTION METHOD
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromSingle=_db.Categories.SingleOrDefault(u=>u.Id == id);
+            //var categoryFromFirst=_db.Categories.FirstOrDefault(u=>u.Id==id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST ACTION METHOD
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            
+           
+        }
     }
 }
